be6d645973c22602cc89e64219909355d96b1248
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2022-09-02 15:53:13 -0600

    Smoke Update

--- FreshSmoke/MATLABFiles/freshSmoke.m
+++ FreshSmoke/MATLABFiles/freshSmoke.m
@@ -4,7 +4,7 @@ clear; close all; clc;
 %% Import Data
 
 %SMPS import
-pathSMPS = 'C:\Users\Thomas\Documents\MATLAB\GitHub\SPUR\CASA_InLab\CASA_InLab\FreshSmoke\DataFiles\fs_08292022';
+pathSMPS = 'C:\Users\Thomas\Documents\MATLAB\GitHub\SPUR\CASA_InLab\CASA_InLab\FreshSmoke\DataFiles';
 smpsData = importSMPS(pathSMPS);
 smpsDataRaw = smpsData;
 
@@ -16,11 +16,29 @@ sizeBins = [smpsData{2,1}]; %The size bins are the same for each scan
 numConc = [smpsData{4,:}];
 
 
+%Calculating the average
+meanConc = mean([smpsData{3,2:end}],2);
+
 %% Plotting
 figure();
-plot([smpsData{2,3}], [smpsData{3,3}]);
+plot([smpsData{2,2}], [smpsData{3,2}]);
+set(gca, 'xscale', 'log');
+set(0, 'defaulttextinterpreter', 'latex');
 hold on
+plot([smpsData{2,3}], [smpsData{3,3}]);
 plot([smpsData{2,4}], [smpsData{3,4}]);
+plot([smpsData{2,5}], [smpsData{3,5}]);
+plot([smpsData{2,6}], [smpsData{3,6}]);
+
+plot([smpsData{2,3}], meanConc, 'linewidth', 3, 'color', 'k');
+
+xlabel('Size $$(nm)$$');
+ylabel('Number Concentration $$\frac{\#}{cm^{3}}$$');
+title('Number Concentration');
+
+%Average Plot
+
+
 
 
 
