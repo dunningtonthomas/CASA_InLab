5988d74d1478c694859ccf8db0855a718af84730
dunningtonthomas <76410702+dunningtonthomas@users.noreply.github.com>
2022-09-09 16:47:21 -0600

    Update

--- FreshSmoke/MATLABFiles/freshSmoke.m
+++ FreshSmoke/MATLABFiles/freshSmoke.m
@@ -13,22 +13,44 @@ smpsDataRaw = smpsData;
 
 %Size Distribution Analysis
 sizeBins = [smpsData{2,1}]; %The size bins are the same for each scan
-numConc = [smpsData{4,:}];
 
 
 %Calculating the average
 meanConc = mean([smpsData{3,2:end}],2);
 
+%Finding the maximum concentration of the mean
+[maxConc, ind] = max(meanConc);
+
+%Finding total number concentration, storing in the fourth row
+for i = 1:length(smpsData(1,:))
+    
+    smpsData{4,i} = sum([smpsData{3,i}]);
+end
+
+%Subtracting the ambient scan initial concentration to get the change once
+%smoke is introduced
+initialConc = sum([smpsData{3,1}]);
+for i = 1:length(smpsData(1,1:end))
+    
+    smpsData{4,i} = sum([smpsData{3,i}]) - initialConc;
+    
+end
+
+
+
+
 %% Plotting
+%Plotting the individual size distribution
 figure();
 plot([smpsData{2,2}], [smpsData{3,2}]);
 set(gca, 'xscale', 'log');
 set(0, 'defaulttextinterpreter', 'latex');
 hold on
-plot([smpsData{2,3}], [smpsData{3,3}]);
-plot([smpsData{2,4}], [smpsData{3,4}]);
-plot([smpsData{2,5}], [smpsData{3,5}]);
-plot([smpsData{2,6}], [smpsData{3,6}]);
+
+for i = 1:length(smpsData(1,3:end))
+    ind = i + 2; %Starting at index 3
+    plot([smpsData{2,ind}], [smpsData{3,ind}]);
+end
 
 plot([smpsData{2,3}], meanConc, 'linewidth', 3, 'color', 'k');
 
@@ -36,7 +58,10 @@ xlabel('Size $$(nm)$$');
 ylabel('Number Concentration $$\frac{\#}{cm^{3}}$$');
 title('Number Concentration');
 
-%Average Plot
+
+%Total number concentration
+figure();
+plot([smpsData{1,:}], [smpsData{4,:}]);
 
 
 
