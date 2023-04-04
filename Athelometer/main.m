50dcc10fc20b9fe75894a4c35485bb0263e4c82e
dunningtonthomas <dunningtonthomas@gmail.com>
2023-04-04 12:30:38 -0600

    Update

--- Athelometer/main.m
+++ Athelometer/main.m
@@ -2,56 +2,46 @@
 clear; close all; clc;
 
 %% Import Data​
-T = readtable('2022_03_22To2022_03_25');
+T1 = readtable('MA200-0030_20220325-044400_post-processed_55911.csv');
+T2 = readtable('MA200-0030_20220325-044400_post-processed_55911.csv');
+T3 = readtable('MA200-0030_20220325-044400_post-processed_55911.csv');
+T4 = readtable('MA200-0030_20220325-044400_post-processed_55911.csv');
+T5 = readtable('MA200-0030_20220325-044400_post-processed_55911.csv');
 
 % What type of waves 
-vars = ["UVBCc", "BlueBCc", "GreenBCc", "RedBCc", "IRBCc"];
-data = T{:,vars};
+vars = ["UVBCcPost", "IRBCcPost"];
+data = T1{:,vars};
 logVec = ~isnan(data);
-data = rmmissing(data); %Getting rid of NAN values
+dataTrunc = rmmissing(data); %Getting rid of NAN values
 
 %Getting time data
-dateOnly = T{:,"DateLocal_yyyy_MM_dd_"};
-timeOnly = T{:,"TimeLocal_hh_mm_ss_"};
+dateOnly = T1{:,"DateLocal"};
+timeOnly = T1{:,"TimeLocal"};
 dateOnly = datetime(dateOnly, 'InputFormat',"yyyy/MM/dd");
 time = dateOnly + timeOnly;
 
 %Truncate time to match with data without nan values
-time = time(logVec(:,1));
+timeLog = (logVec(:,1) .* logVec(:,2)) == 1;
+time = time(timeLog);
 
-meanOfWaveLength = mean(T{:,vars}, 'omitnan');
-meanOfWaveLength = meanOfWaveLength';
-% Wavelengths
-waveLengths = [375,470,528,625,880]';
 
 %Subtract IRBcc from UVBCc to get brown carbon, normalize with UVBCc
 %This gives you an approximate amount of brown carbon in the sample
-
-
-plotData = table(waveLengths, meanOfWaveLength);
-
+%Use the POST data at the last columns in the data files
 
 %% Analysis
 
 
 
 %% Plotting
-plot(plotData.waveLengths,plotData.meanOfWaveLength)
-title('WaveLength vs Amount of Black Carbon')
-xlabel('Wavelength (nm)')
-ylabel('Absoprtion')
-
-
 %Plotting the wavelength concentrations over time
 figure();
-plot(time, data(:,1));
+plot(time, dataTrunc(:,1));
 hold on
-plot(time, data(:,2));
-plot(time, data(:,3));
-plot(time, data(:,4));
+plot(time, dataTrunc(:,2));
 
 xlabel('Time');
 ylabel('Absorption');
-legend('UVBCc', 'BlueBCc', 'GreenBCc', 'RedBCc', 'IRBCc');
+legend('UVBCc', 'IRBCc');
 
 
